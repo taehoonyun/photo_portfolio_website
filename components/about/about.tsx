c2c60626bf76007bf06dce466114c1646ef9edc6
taehoonYun <mnmn9069@gmail.com>
2024-09-22 16:56:29 -0400

    removed prettier

--- components/about/about.tsx
+++ components/about/about.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
 import style from "./about.module.css";
-import photos from "../../public/photos.json";
 import { useRouter } from "next/router";
 
 export default function About() {
@@ -18,18 +17,17 @@ export default function About() {
     <div className="flex flex-col md:p-20 basis-1/2 m-10 md:m-12">
       <span className="text-lg font-bold mb-4 text-[32px] md:text-2xl">ABOUT</span>
       <span className="leading-8 md:leading-10 text-xl md:text-xl text-justify">
-        asd tyughj is a rtyfh photographer uisqsz pn utyhyuo, jhklqmz to
-        hgfh the hgfh and zxczxs of ffaaasdwas qweqq erg gerge.
-        fghfgh by the fqwxghte of optnvyq, nature, and eeeff dfgdreer, her
-        qjcmxcv wek isdieo mcvbkld mdflm zq, kdmb. wieur, sdkjwiq, skdflz
-        qiojds,fm relktler mer; kerljeorikmd mle le lsk eo azoeirjmn xsqa
-        work qggh to fghfg usaqvl wqe mfjfjq fine asdas iotosqnz1.
+        Sarah Yun is a versatile photographer based in Atlanta, seeking to
+        document the splendor and allure of creation through honest imagery.
+        Inspired by the interplay of light, nature, and human complexity, her
+        work seeks to intertwine visual storytelling with fine art expression.
       </span>
     </div>
     <div className="basis-1/2 flex justify-center mt-8 md:mt-0">
       <Image
         className="cursor-pointer"
-        src={photos.portfolio_main
+        src={
+          "https://res.cloudinary.com/dxk7brqop/image/upload/v1726350774/IMG_2255_xnrxoj.jpg"
         }
         alt="Picture of the author"
         width={650}
@@ -37,7 +35,7 @@ export default function About() {
         quality={100}
         sizes="100vw"
         style={{
-          width: "75%",
+          width: "90%",
           height: "auto",
           minWidth: "300px",
           minHeight: "200px",
